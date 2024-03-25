c425099fa207c710305d6cca02db9d2dac2814f3
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2024-03-25 22:29:29 +0300

    add the Hello World program in C

---
+++ 01-hello.c
@@ -0,0 +1,11 @@
+/*
+ * The Hello World Program in C
+ */
+#include <math.h>
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  printf("sqrt(4) is %f\n", sqrt(4));
+  return 0;
+}
